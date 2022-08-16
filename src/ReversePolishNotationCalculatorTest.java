e183933db19812fa84ebe3971d29e6f63c106606
Slava <40612104+UserOne@users.noreply.github.com>
2022-08-16 09:37:14 +0300

    Я бы вынес создание экземпляра калькулятора в @BeforeAll.
    Нужно ещё проверить работоспособность на отрицательных числах, а так-же добавить проверку на расчет более 2 символов и 1 знака.

--- src/ReversePolishNotationCalculatorTest.java
+++ src/ReversePolishNotationCalculatorTest.java
@@ -1,33 +1,37 @@
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
 
 public class ReversePolishNotationCalculatorTest {
 
+    private static ReversePolishNotationCalculator reversePolishNotationCalculator;
+    
+    @BeforeAll
+    public static void beforeAll(){
+        reversePolishNotationCalculator = new ReversePolishNotationCalculator();
+    }
+    
     @Test
     public void shouldCalculateAddition() {
-    ReversePolishNotationCalculator reversePolishNotationCalculator = new ReversePolishNotationCalculator();
     int result = reversePolishNotationCalculator.calculatePolishNotation("2 3 +");
     Assertions.assertEquals(5, result);
     }
 
     @Test
     public void shouldCaclulateMinus() {
-        ReversePolishNotationCalculator reversePolishNotationCalculator = new ReversePolishNotationCalculator();
         int result = reversePolishNotationCalculator.calculatePolishNotation("5 4 -");
         Assertions.assertEquals(1, result);
     }
 
     @Test
     public void shouldCaclulateMultiplication() {
-        ReversePolishNotationCalculator reversePolishNotationCalculator = new ReversePolishNotationCalculator();
         int result = reversePolishNotationCalculator.calculatePolishNotation("2 3 *");
         Assertions.assertEquals(6, result);
     }
 
     @Test
     public void shouldCaclulateSpaces() {
-        ReversePolishNotationCalculator reversePolishNotationCalculator = new ReversePolishNotationCalculator();
         int result = reversePolishNotationCalculator.calculatePolishNotation(" 6    2  - ");
         Assertions.assertEquals(4, result);
     }
